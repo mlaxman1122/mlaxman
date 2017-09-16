00ac909f7ab2ec765dbcc5fa11dfdae2d4e74dbc
mlaxman1122 <32007849+mlaxman1122@users.noreply.github.com>
2017-09-16 12:34:11 +0530

    Add files via upload

---
+++ src/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+		StudentGroup p=new StudentGroup(10);
+		Date l=new Date(1996,9,23);
+		Student a=new Student(1,"laxman",new Date(1996,9,22),50);
+		Student b=new Student(1,"lucky",new Date(1996,10,22),50);
+        Student c=new Student(1,"lakshman",new Date(1996,11,22),50);
+        system.out.println(getid());
+
+	}
+
+}
